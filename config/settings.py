bf3ff161c88043ead07f4f3c70f8caa6558f62f7
bilalseymen <seymen97@gmail.com>
2021-12-27 11:06:57 +0300

    static files

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-j5ed!&4lfz!eixxu!$h@*3b-n@frm56(45z3-vjm!+w5p3wd4q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['194.147.58.159', 'www.symnsoft.com', 'symnsoft.com']
 
@@ -120,6 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
+# STATIC_ROOT = '/home/ubuntu/django_blog_udemy/static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
